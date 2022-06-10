cc12b31fdc4f5476b2f22d22a62335c1bf5e749f
thompsongwill <thompsongodswill@gmaill.com>
2022-06-10 14:14:12 +0100

    post test

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,7 +5,12 @@ from datetime import datetime
 # Create your models here.
 
 class Post(models.Model):
-    tile = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    sub_title = models.CharField(max_length=500, blank=True)
     body = models.TextField()
     image = models.ImageField(upload_to="media/images")
     created = models.DateTimeField(default=datetime.now, blank=True)
+    
+    
+    def __str__(self):
+        return self.title

bf19f86d05130df7661ac3453a5a9c08990bddf6
thompsongwill <thompsongodswill@gmaill.com>
2022-06-10 15:46:19 +0100

    working on detail page

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('post/<int:pk>', views.single_post, name='post'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -9,3 +9,9 @@ def index(request):
     posts = Post.objects.all()
     context = {"post":posts}
     return render(request, 'index.html', context)
+
+
+def single_post(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {'posts':post}
+    return render(request, 'post.html', context)

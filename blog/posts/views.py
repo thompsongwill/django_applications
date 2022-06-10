a4d8bc8241738c176fdb58ac885bee9da91433cb
thompsongwill <thompsongodswill@gmaill.com>
2022-06-10 14:38:58 +0100

    displaying post content

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from . models import Post
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    
+    posts = Post.objects.all()
+    context = {"post":posts}
+    return render(request, 'index.html', context)

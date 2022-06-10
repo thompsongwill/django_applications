fdb3665a1a810b83eb063803308e69a1d076350a
thompsongwill <thompsongodswill@gmaill.com>
2022-06-10 12:37:43 +0100

    created posts model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,11 @@
+from distutils.command.upload import upload
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Post(models.Model):
+    tile = models.CharField(max_length=150)
+    body = models.TextField()
+    image = models.ImageField(upload_to="media/images")
+    created = models.DateTimeField(default=datetime.now, blank=True)

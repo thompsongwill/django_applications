e389dd99ee8eaf7c930928e84e9ae27930f99ede
thompsongwill <thompsongodswill@gmaill.com>
2022-06-11 23:49:21 +0100

    crypto compare API integration

--- crypto_news/crypto/views.py
+++ crypto_news/crypto/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'crypto/index.html', {})
+    #Crypto Prices data data/pricemultifull?fsyms
+    api_price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX,ETH&tsyms=USD")
+    api_price = json.loads(api_price_request.content)
+    
+    # Crypto News 
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request, 'crypto/index.html', {'api':api, 'price':api_price})
+
+
+def prices(request):
+    if request.method == 'POST':
+        quote = request.POST['quote']
+        crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD")
+        api_quote = json.loads(crypto_request.content)
+        
+        
+      
+        return render(request, 'crypto/prices.html', {"quote":quote, 'crypto':api_quote})
+    
+    else:
+        return render(request, 'crypto/prices.html', {})
+    

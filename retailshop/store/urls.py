45d4c0d620aa728392dfb1b0506b34cd82af375c
thompsongwill <thompsongodswill@gmaill.com>
2022-06-16 15:29:21 +0100

    creating form for adding new product

--- retailshop/store/urls.py
+++ retailshop/store/urls.py
@@ -9,6 +9,7 @@ from . import views
 urlpatterns = [ 
               path('', views.index, name='home'),
               path('product/<str:product_id>', views.details, name='details'),
+              path('addproduct/', views.add_product, name='add_product'),
               ]
 
 
--- retailshop/store/views.py
+++ retailshop/store/views.py
@@ -17,3 +17,17 @@ def details(request,product_id):
     product = Product.objects.get(id=product_id)
     
     return render(request, 'store/details.html', {'product':product})
+
+
+def add_product(request):
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        price = request.POST.get('price')
+        desc = request.POST.get('desc')
+        image = request.FILES['upload']
+        
+        product = Product(title=title, price=price, des=desc, image=image)
+        product.save()
+    
+    return render(request, 'store/addproduct.html')

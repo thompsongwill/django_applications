07a0cde5e0f987f4a1f01ae86f2f85c6f5836912
thompsongwill <thompsongodswill@gmaill.com>
2022-06-16 10:28:53 +0100

    online retail store project; first commit

---
+++ retailshop/retailshop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for retailshop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retailshop.settings')
+
+application = get_wsgi_application()

58d5563ff58cbd44c3d02b14a4aed4476dade6c9
thompsongwill <thompsongodswill@gmaill.com>
2022-06-17 17:50:12 +0100

    costumizing the admin dashboard

--- retailshop/store/admin.py
+++ retailshop/store/admin.py
@@ -4,4 +4,24 @@ from .models import Product
 # Register your models here.
 
 
-admin.site.register(Product)
+admin.site.site_header = "Retail Store Admin"
+admin.site.index_title = "Retail Admin Dashboard"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('seller','title','price','image')
+    search_fields = ('title',)
+    
+    def set_price_to_zeero(self,request,queryset):
+        queryset.update(price=0)
+        
+    actions = ('set_price_to_zero',)
+    
+    # list_editable = ('title','price')
+    
+
+
+
+
+
+admin.site.register(Product,ProductAdmin)
